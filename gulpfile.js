14e61e8dd1287214347928c4478826188966f66b
Dmitry <z@annexare.com>
2014-08-26 02:18:27 +0300

    v1.0.1 RU/UA detect for "и"; build version

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,9 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     moment = require('moment');
 
-var banner = function () { return '/*! String.toURI() by Annexare.com, ' + moment().format('YYYY.MM.DD') + ' */\n';},
-    src = 'toURI.js';
+var pkg = require('./package.json'),
+    banner = function () { return '/*! String.toURI() by Annexare.com, v' + pkg.version + ' ' + moment().format('YYYY.MM.DD') + ' */\n';},
+    src = 'lib/toURI.js';
 
 gulp.task('uglify', function() {
     gulp.src(src)
